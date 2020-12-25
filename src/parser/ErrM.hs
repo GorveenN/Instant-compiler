58ffd5885205461373d9305be9d4f9d519e74420
Piotr Karpiński <gorveenn@gmail.com>
2020-12-25 23:26:16 +0100

    Parser

--- src/parser/ErrM.hs
+++ src/parser/ErrM.hs
@@ -6,22 +6,23 @@ module ErrM where
 
 -- the Error monad: like Maybe type with error msgs
 
-import Control.Applicative (Alternative (..), Applicative (..))
-import Control.Monad (MonadPlus (..), liftM)
+import Control.Monad (MonadPlus(..), liftM)
+import Control.Applicative (Applicative(..), Alternative(..))
 
 data Err a = Ok a | Bad String
   deriving (Read, Show, Eq, Ord)
 
 instance Monad Err where
-  return = Ok
-  fail = Bad
-  Ok a >>= f = f a
+  return      = Ok
+  fail        = Bad
+  Ok a  >>= f = f a
   Bad s >>= _ = Bad s
 
 instance Applicative Err where
   pure = Ok
   (Bad s) <*> _ = Bad s
-  (Ok f) <*> o = liftM f o
+  (Ok f) <*> o  = liftM f o
+
 
 instance Functor Err where
   fmap = liftM
@@ -29,7 +30,7 @@ instance Functor Err where
 instance MonadPlus Err where
   mzero = Bad "Err.mzero"
   mplus (Bad _) y = y
-  mplus x _ = x
+  mplus x       _ = x
 
 instance Alternative Err where
   empty = mzero
