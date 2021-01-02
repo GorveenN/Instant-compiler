50da171002abc53bb6ff76ac9af466efdc4da3e9
Piotr Karpiński <gorveenn@gmail.com>
2021-01-02 02:57:12 +0100

    Add command args

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -20,14 +20,15 @@ import System.FilePath
 import System.IO (hPrint, hPutStrLn, stderr)
 import System.Process
 
-type ParseFun a = [Token] -> Err a
-
-myLLexer = myLexer
-
 type Verbosity = Int
 
 putStrV :: Verbosity -> String -> IO ()
 putStrV v s = when (v > 1) $ putStrLn s
+
+showTree :: (Show a, Print a) => Int -> a -> IO ()
+showTree v tree = do
+  putStrV v $ "\n[Abstract Syntax]\n\n" ++ show tree
+  putStrV v $ "\n[Linearized tree]\n\n" ++ printTree tree
 
 -- runFile v p f = readFile f >>= run v p
 runFile p f = readFile f >>= run p f
@@ -57,51 +58,58 @@ run p n s =
           -- hPutStrLn stderr "Ok"
           exitSuccess
 
-showTree :: (Show a, Print a) => Int -> a -> IO ()
-showTree v tree = do
-  putStrV v $ "\n[Abstract Syntax]\n\n" ++ show tree
-  putStrV v $ "\n[Linearized tree]\n\n" ++ printTree tree
-
 main :: IO ()
 main = do
   args <- getArgs
   mapM_ (runFile pProgram) args
 
--- data Sample = Sample
---   { output :: String,
---     library :: String,
---     quiet :: Bool,
---     asm :: Bool,
---     arg :: String
---   }
---   deriving (Show)
+_run :: Args -> IO ()
+_run args = do
+  fileContent <- readFile $ arg args
+  case pProgram (myLexer fileContent) of
+    Bad s -> do
+      hPutStrLn stderr "ERROR"
+      hPutStrLn stderr "Parse Error"
+      hPrint stderr $ show s
+      exitFailure
+      return ()
+    Ok tree -> do
+      case runSRE tree of
+        (Left exc) -> do
+          hPutStrLn stderr "ERROR"
+          hPutStrLn stderr ("Static check failed\n" ++ show exc)
+          exitFailure
+        (Right ()) -> do
+          let n = arg args
+          let dots = replaceExtension n "s"
+          let doto = replaceExtension n "o"
+          let base = dropExtension n
+          let prog = cumpile $ transProgram tree
+          writeFile dots $ unlines prog
+          callCommand "mkdir -p .build"
+          callCommand $ "gcc -g -m32 -o .build/runtime.o -c " ++ library args
+          callCommand $ "gcc -g -m32 -o .build/" ++ takeBaseName (arg args) ++ ".s -c " ++ dots
+          callCommand $ "gcc -g -m32 -o " ++ base ++ " " ++ doto ++ " build/runtime.o"
+          -- hPutStrLn stderr "Ok"
+          exitSuccess
 
--- sample :: Parser Sample
--- sample =
---   Sample
---     <$> strOption
---       ( long "output"
---           <> short 'o'
---           <> metavar "TARGET"
---           <> help "Output path"
---       )
---     <*> strOption
---       ( long "library"
---           <> short 'l'
---           <> value "./build/runtime.c"
---           <> help "Whether to be quiet"
---       )
---     <*> switch
---       ( long "quiet"
---           <> short 'q'
---           <> help "Whether to be quiet"
---       )
---     <*> switch
---       ( long "asm"
---           <> short 'a'
---           <> help "Whether to emmit asm file before compiling"
---       )
---     <*> argument str (help "ala ma kota")
+data Args = Args
+  { output :: Maybe String,
+    library :: String,
+    quiet :: Bool,
+    asm :: Bool,
+    arg :: String
+  }
+  deriving (Show)
+
+sample :: Parser Args
+sample =
+  Args
+    <$> optional (strOption $ long "output" <> short 'o')
+    <*> strOption (long "library" <> short 'l' <> value "./stdlib/runtime.c")
+    <*> switch (long "quiet" <> short 'q')
+    <*> switch (long "asm" <> short 'a')
+    <*> argument str (help "ala ma kota")
 
 -- main :: IO ()
 -- main = execParser opts >>= greet
@@ -113,6 +121,3 @@ main = do
 --             <> progDesc "Print a greeting for TARGET"
 --             <> header "hello - a test for optparse-applicative"
 --         )
-
--- greet :: Sample -> IO ()
--- greet = print
