64565ab8bcee7dca9b1f09a8d4a36021a375eae1
Piotr Karpiński <gorveenn@gmail.com>
2021-01-07 01:43:33 +0100

    Update makepkg script

--- makepkg.sh
+++ makepkg.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-tar czvf pk394334.tar.gz stdlib src package.yaml stack.yaml Makefile README.md
+rm -rf pk394334
+mkdir pk394334
+cp -r stdlib src package.yaml stack.yaml Makefile README.md lattests pk394334
+rm -rf pk394334/lattests/others
+cd pk394334
+tar czvf ../pk394334.tar.gz *
+cd ..
+rm -rf pk394334
